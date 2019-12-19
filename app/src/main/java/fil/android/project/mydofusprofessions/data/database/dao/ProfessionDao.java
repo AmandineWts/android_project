698d495dfed9cbba6e9f6e2f28530740ffaa8324
Amandine Watrelos <amandine.watrelos@gmail.com>
2019-12-19 20:03:45 +0100

    Fix pour compilation

--- app/src/main/java/fil/android/project/mydofusprofessions/data/database/dao/ProfessionDao.java
+++ app/src/main/java/fil/android/project/mydofusprofessions/data/database/dao/ProfessionDao.java
@@ -1,6 +1,7 @@
 package fil.android.project.mydofusprofessions.data.database.dao;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
 
 import fil.android.project.mydofusprofessions.data.database.entity.ProfessionEntity;
 import io.reactivex.Completable;
@@ -8,6 +9,7 @@ import io.reactivex.Completable;
 @Dao
 public interface ProfessionDao {
 
+    @Insert
     Completable insert(ProfessionEntity item);
 
 }
--- app/src/main/java/fil/android/project/mydofusprofessions/data/database/entity/ProfessionEntity.java
+++ app/src/main/java/fil/android/project/mydofusprofessions/data/database/entity/ProfessionEntity.java
@@ -1,8 +1,16 @@
 package fil.android.project.mydofusprofessions.data.database.entity;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName="items")
 public class ProfessionEntity {
+
+    @PrimaryKey
+    @ColumnInfo(name = "ankama_id")
+    @NonNull
+    public String ankamaId;
 
 }
