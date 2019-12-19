0f4ea1ac0063cef2976a1f3cb4d03cee17d9e6d1
Amandine Watrelos <amandine.watrelos@gmail.com>
2019-12-19 19:09:39 +0100

    Changement de nom du projet

---
+++ app/src/main/java/fil/android/project/mydofusprofessions/presentation/ProfessionsDisplayActivity.java
@@ -0,0 +1,8 @@
+package fil.android.project.mydofusprofessions.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfessionsDisplayActivity extends AppCompatActivity {
+
+
+}
